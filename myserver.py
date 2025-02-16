78c19b3bb22123358890e9ae05b4ec4a9f55d477
XBank2004 <chayutrachaodee@gmail.com>
2025-02-16 17:55:59 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)  # ใช้ __name__ แทน ''
 
 @app.route('/')
-def home ():
+def home():
     return "Server Is Running!"
 
 def run():
-    app.run(host='0.0.0.0',port=8080)
-    
+    # ใช้ app.run() โดยกำหนดให้รองรับการเข้าถึงจากทุกที่
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
 def server_on():
-    t = Thread(target=run)
-    t.start()    
+    t = Thread(target=run)  # สร้าง thread สำหรับการรัน Flask
+    t.start()  # เริ่มต้นการทำงานของ thread
+
+# เรียกฟังก์ชัน server_on() เพื่อเริ่มการทำงานของเซิร์ฟเวอร์
+if __name__ == "__main__":
+    server_on()  # เริ่มต้นเซิร์ฟเวอร์
